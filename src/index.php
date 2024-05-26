5b808cb37f919bc08807b7fe8e27a126a03a7dc6
GaborVxxx <gaborvtattoo@gmail.com>
2024-05-26 10:52:08 +0100

    working conection to MySql docker container

--- src/index.php
+++ src/index.php
@@ -1,17 +1,28 @@
 <?php
 /** PDO (PHP Data Objects) */
-// todo... use Docker to create a docker mysql data base and connect to it from the project!
 
-$servername = "mysql_db";
-$username = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
-$dbname = getenv('MYSQL_DATABASE');
+// docker-compose up --build  <-- command to build the container for database and run it. NOTE: require Docker!
+// docker-compose up  <-- if container already exist...
+// url: http://localhost:8080/src  <-- navigate to url to see the connection.
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$env = parse_ini_file('../.env'); // pars the .env from root dir
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$host = "127.0.0.1";
+$port = 3306;
+$dbName = $env['MYSQL_DATABASE'];
+$username = $env['MYSQL_USER'];
+$password = $env['MYSQL_PASSWORD'];
+
+$dsn = "mysql:host={$host};port={$port};dbname={$dbName};charset=utf8";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+
+    // set it to throw exceptions on error
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    // errors
+    echo $e->getMessage();
 }
-echo "Connected successfully";
